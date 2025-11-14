a5c407c6b901247c4b42a06dcd7f32f92d4ebb26
ali salem essouiah <contact@aliess.me>
2025-11-14 12:32:39 +0100

    checkpoint

--- pdti_frontend/src/config/constants/apiConfig.ts
+++ pdti_frontend/src/config/constants/apiConfig.ts
@@ -2,5 +2,5 @@ export const apiBaseUrl =
   import.meta.env.FRONTEND_API_URL || "https://api.pdti-essemara.ma/api";
 /*xport const apiBaseUrl =
   import.meta.env.FRONTEND_API_URL || "http://localhost:4003/api";*/
-export const apiIntegrationKey =
-  "$2a$12$o.xa1omOe0ijNlJCsHqVV.IxzZYeWdlHuVTyS6sd4MhEuIdqZPjCq";
+export const apiIntegrationKey = "c5069844-6351-4191-90f7-fdad5021cc84";
+//$2a$12$o.xa1omOe0ijNlJCsHqVV.IxzZYeWdlHuVTyS6sd4MhEuIdqZPjCq
